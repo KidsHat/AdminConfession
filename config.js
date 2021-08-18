b0db4f80ebc11d55b8266862f559efb075b13ffb
KidsHat <89117861+KidsHat@users.noreply.github.com>
2021-08-18 20:15:50 +0700

    a

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello mấy bạn xink đẹp của ad',
+    introDesc: `Ad muốn hỏi cái này nèèèèèè`,
+    btnIntro: 'Nhấn đi Ad thương nè 😘😘',
+    title: 'Bạn có thấy ad xink đẹp cute hong dọ😙',
+    desc: 'Trả lời thật lòng nhah !! ',
+    btnYes: 'Có Ad xink đẹp quáaaaaa',
+    btnNo: 'Ad không có xink gì hết',
+    question:'Cảm nghĩ của bạn về ad😙 ',
+    btnReply: 'Gửi cho Ad dễ thương 😘😘',
+    reply: 'Ad là 1 người vừa xink đẹp vừa dễ thương em sẽ ủng hộ page này ạ !!!!😘😘',
+    mess: 'Admin Confession cảm ơn bạn 😘😘 ',
+    messDesc: 'Nếu có việc gì muốn bày tỏ cảm xúc đừng ngại mà nt cho Ad nhé 😘',
+    btnAccept: 'Bấm vào để đồng ý',
+    messLink: 'https://www.facebook.com/NBKconfessionAG' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
